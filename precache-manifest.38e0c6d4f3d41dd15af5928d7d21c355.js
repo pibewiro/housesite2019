60144ef885d7f81c9e6bc207f4ea118ac5f1a3f1
pibewiro <pibewiro@yahoo.com>
2019-04-29 18:46:37 -0300

    Updates

---
+++ precache-manifest.38e0c6d4f3d41dd15af5928d7d21c355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4a9bcb8bd11c70edb0ec350eb3cbd2",
+    "url": "/housesite2019/index.html"
+  },
+  {
+    "revision": "ffc1845b23b49be99137",
+    "url": "/housesite2019/static/css/main.e1b71581.chunk.css"
+  },
+  {
+    "revision": "00a951e0b3e7e1c1b546",
+    "url": "/housesite2019/static/js/2.e3013ac0.chunk.js"
+  },
+  {
+    "revision": "ffc1845b23b49be99137",
+    "url": "/housesite2019/static/js/main.801cf3c5.chunk.js"
+  },
+  {
+    "revision": "543ad0e5b15e721090a0",
+    "url": "/housesite2019/static/js/runtime~main.ebdf5078.js"
+  },
+  {
+    "revision": "ec7fc144cafc4d64d60186be98c5b564",
+    "url": "/housesite2019/static/media/homeImage.ec7fc144.png"
+  }
+]);
